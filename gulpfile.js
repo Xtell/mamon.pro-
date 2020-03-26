73ec1a47d03e67a0c5d4249a32162e2b015c9c0f
xtell <evstafyevan96@gmail.com>
2020-03-26 20:14:48 +0500

    Add desktop styles of header

---
+++ gulpfile.js
@@ -0,0 +1,116 @@
+let gulp            = require('gulp'),
+    sass            = require('gulp-sass'),
+    csso            = require('gulp-csso'),
+    postcss         = require('gulp-postcss'),
+    autoprefixer    = require('autoprefixer'),
+    imagemin        = require('gulp-imagemin'),
+    webp            = require('imagemin-webp'),
+    svgo            = require('gulp-svgmin'),
+    svgSprite       = require('gulp-svg-sprite'),
+    cache           = require('gulp-cache'),
+    uglify          = require('gulp-uglify-es').default,
+    rigger          = require('gulp-rigger'),
+    newer           = require('gulp-newer'),
+    rename          = require('gulp-rename'),
+    concat          = require('gulp-concat'),
+    del             = require('del'),
+    browserSync     = require('browser-sync').create(),
+    debug           = require('gulp-debug');
+
+let paths = {
+    'scss': './app/scss/',
+    'css': './app/css/',
+    'scripts': './app/js/',
+    'imagesSrc': './app/img/src/',
+    'imagesDest': './app/img/',
+    'html': './app/html/*.html'
+
+}
+
+gulp.task('serve', function(done) {
+    browserSync.init({
+        server: "./app"
+    });
+});
+
+gulp.task('styles', function () {
+    return gulp.src(paths.scss + 'main.scss')
+    .pipe(sass())
+    .pipe(postcss([ autoprefixer() ]))
+    .pipe(csso())
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest(paths.css))
+    .pipe(browserSync.stream());
+});
+gulp.task('scripts', function() {
+    return gulp.src([
+        paths.scripts + 'libs.js',
+        paths.scripts + 'custom.js'
+    ])
+    .pipe(concat('script.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.scripts))
+    .pipe(browserSync.stream());
+});
+
+//IMAGES OPTIMIZATION
+
+gulp.task('jpegAndPngOptimize', function() {
+    return gulp.src(paths.imagesSrc + '**/*')
+    .pipe(cache(
+
+    ))
+    .pipe(imagemin([
+           imagemin.mozjpeg({quality: 75, progressive: true}),
+           imagemin.optipng({optimizationLevel: 3})
+        ]))
+	.pipe(gulp.dest(paths.imagesDest));
+});
+gulp.task('jpegOptimize', function() {
+    return gulp.src(paths.imagesSrc + '**/*.{jpg, jpeg}')
+    .pipe(imagemin([
+        imagemin.mozjpeg({quality: 75, progressive: true})
+    ]))
+    .pipe(gulp.dest(paths.imagesDest));
+});
+gulp.task('pngOptimize', function() {
+    return gulp.src(paths.imagesSrc + '**/*.png')
+    .pipe(imagemin([
+        imagemin.optipng()
+    ]))
+    .pipe(gulp.dest(paths.imagesDest));
+});
+gulp.task('makeWebp', function() {
+    return gulp.src(paths.imagesSrc + '**/*.{png,jpg,jpeg}')
+    .pipe(imagemin([
+        webp({quality: 80})
+    ]))
+    .pipe(gulp.dest(paths.imagesDest));
+});
+
+gulp.task('optimizeSvg', function() {
+    return gulp.src(paths.imagesSrc + '**/*.svg')
+    .pipe(imagemin([
+        imagemin.svgo({
+            plugins: [
+                {removeViewBox: true},
+                {cleanupIDs: false}
+            ]
+        })
+    ]))
+    .pipe(gulp.dest(paths.imagesDest));
+});
+gulp.task('images', gulp.series('makeWebp', 'jpegOptimize','pngOptimize', 'optimizeSvg'));
+
+gulp.task('html', function () {
+    return gulp.src(paths.html)
+    .pipe(gulp.dest('./app'))
+    .pipe(browserSync.stream());
+});
+gulp.task('watch', function(){
+    gulp.watch(paths.scss + '**/*', gulp.series('styles'));
+    gulp.watch(paths.html, gulp.series('html'));
+    gulp.watch([paths.scripts + 'libs.js', paths.scripts + 'custom.js'], gulp.series('scripts'));
+});
+
+gulp.task('default', gulp.parallel('images', 'styles', 'scripts', 'html', 'serve', 'watch'));
